799cfe0078d9842ce0389d8f531fad3bd0213e1c
qrzeller <pttptt3*>
2017-05-31 16:59:00 +0200

    Example RMI$

---
+++ RMI-Interfaces/src/main/java/rmi/IClientApi.java
@@ -0,0 +1,16 @@
+package rmi;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * @author https://sites.google.com/site/jamespandavan/Home/java/sample-remote-observer-based-on-rmi
+ */
+public interface IClientApi extends Remote {
+	
+	public enum Signal {
+		UPDATE_REQUESTED
+	}
+	
+	void update(Object observable, Signal signalType, String updateMsg) throws RemoteException;
+}
