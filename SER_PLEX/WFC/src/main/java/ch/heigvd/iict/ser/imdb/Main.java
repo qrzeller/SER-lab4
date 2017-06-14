7494033ea19e1e5168c42029b18754a1ae008e2b
qrzeller <quentin.zeller@epfl.ch>
2017-06-14 12:01:24 +0200

    main

--- SER_PLEX/WFC/src/main/java/ch/heigvd/iict/ser/imdb/Main.java
+++ SER_PLEX/WFC/src/main/java/ch/heigvd/iict/ser/imdb/Main.java
@@ -1,58 +1,141 @@
 package ch.heigvd.iict.ser.imdb;
 
+import java.io.Serializable;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Observable;
+import java.util.Observer;
 import java.util.Scanner;
 
 import ch.heigvd.iict.ser.imdb.db.MySQLAccess;
 import ch.heigvd.iict.ser.imdb.models.Data;
+import ch.heigvd.iict.ser.rmi.IClientApi;
+import ch.heigvd.iict.ser.rmi.IServerApi;
+
 
 public class Main {
 
-	static {
-		// this will load the MySQL driver, each DB has its own driver
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
-			System.err.println("MySQL drivers not found !");
-			System.exit(1);
-		}
+    //Ajout de l'objet serveur RMI --> inner class
+    private customRMIServer serverRMI = new customRMIServer(this);
 
-		//database configuration
-		MySQLAccess.MYSQL_URL 		= "docr.iict.ch";
-		MySQLAccess.MYSQL_DBNAME 	= "imdb";
-		MySQLAccess.MYSQL_USER 		= "imdb";
-		MySQLAccess.MYSQL_PASSWORD 	= "imdb";
-	}
+    static {
+        // this will load the MySQL driver, each DB has its own driver
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.err.println("MySQL drivers not found !");
+            System.exit(1);
+        }
 
-	private static Scanner scanner = new Scanner(System.in);
+        //database configuration
+        MySQLAccess.MYSQL_URL 		= "docr.iict.ch";
+        MySQLAccess.MYSQL_DBNAME 	= "imdb";
+        MySQLAccess.MYSQL_USER 		= "imdb";
+        MySQLAccess.MYSQL_PASSWORD 	= "imdb";
+    }
 
-	public static void main(String[] args) {
-		Main main = new Main();
-		main.run();
-	}
+    private static Scanner scanner = new Scanner(System.in);
 
-	private Data lastData = null;
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.run();
+    }
 
-	private void run() {
+    private Data lastData = null;
 
-		boolean continuer = true;		
-		while(continuer) {
-			System.out.print("Select the data version to download [1/2/3/0=quit]: ");
-			int choice = -1;
-			try {
-				choice = scanner.nextInt();
-			} catch(Exception e) {
-				e.printStackTrace();
-			}
-			
-			if(choice == 0) continuer = false;
-			else if(choice >= 1 && choice <= 3) {
-				Worker worker = new Worker(choice);
-				this.lastData = worker.run();
+    private void run() {
 
-				//TODO notify client
-				
-			}
-		}
-	}
+        boolean continuer = true;
+        while(continuer) {
+            System.out.print("Select the data version to download [1/2/3/0=quit]: ");
+            int choice = -1;
+            try {
+                choice = scanner.nextInt();
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
 
+            if(choice == 0) continuer = false;
+            else if(choice >= 1 && choice <= 3) {
+                Worker worker = new Worker(choice);
+                this.lastData = worker.run();
+
+                //TODO notify client
+
+            }
+        }
+    }
+
+    private class customRMIServer extends Observable implements IServerApi{
+
+
+        private Main main;
+
+        public customRMIServer(Main main) {
+
+            this.main = main;
+            try {
+                Registry rmiRegistry = LocateRegistry.createRegistry(9999);
+                IServerApi rmiService = (IServerApi) UnicastRemoteObject.exportObject(this, 9999);
+                rmiRegistry.bind("RMI_Service", rmiService);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+
+
+        public void setChanged(){
+            super.setChanged();
+        }
+
+
+    /*
+     * API - RmiService implementation
+     */
+
+        //@Override
+        public void addObserver(IClientApi client) throws RemoteException {
+            WrappedObserver wo = new WrappedObserver(client);
+            addObserver(wo);
+        }
+
+        //@Override
+        public boolean isStillConnected() throws RemoteException {
+            return true;
+        }
+
+        //@Override
+        public Data getData() throws RemoteException {
+
+            //TODO remove main and recover lastdata from super
+            return main.lastData;
+        }
+
+        /*
+         *  Observer
+         */
+        private class WrappedObserver implements Observer, Serializable {
+
+            private static final long serialVersionUID = -2067345842536415833L;
+
+            private IClientApi ro = null;
+
+            public WrappedObserver(IClientApi ro) {
+                this.ro = ro;
+            }
+
+            //@Override
+            public void update(Observable o, Object arg) {
+                try {
+                    ro.update(o.toString(), IClientApi.Signal.UPDATE_REQUESTED, arg.toString());
+                } catch (RemoteException e) {
+                    System.out.println("Remote exception removing observer: " + this);
+                    o.deleteObserver(this);
+                }
+            }
+        }
+    }
+}
 }
